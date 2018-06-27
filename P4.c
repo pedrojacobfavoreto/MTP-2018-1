fd4546a86387e5e3550b479b3a3125082406178a
pedrojacobfavoreto <38017495+pedrojacobfavoreto@users.noreply.github.com>
2018-06-27 09:23:19 -0300

    Update P4.c

--- P4.c
+++ P4.c
@@ -1,25 +1,19 @@
-//P4
 //Pedro Jacob Favoreto
 //11721EAU003
 
-# include <stdio.h>
-
-int main() 
+#include <stdio.h>
+int main()
 {
-int cum=0, i;
-char numero[256];
-
-printf ("Digite um numero: ");
-scanf ("%s", &numero);
-getchar ();
-for (i=0; numero[i] != '\0'; i++)
-{
-if (numero[i] >=48 && numero [i] <= 57)
-{
-numero[i] = numero[i] - '0';
-cum = cum*10 + numero[i];
-}
-}
-printf ("\nO numero digitado: %d", cum);
-return 0;
+	int conv=0, cum=0, i=0;
+	char numero[256];
+	scanf("%s", numero);
+	getchar();
+	for (i=0; numero[i]; i++)
+	{
+		if ((numero[i]-'0')>=0 && (numero[i]-'0')<=9)
+			cum=cum*10+(numero[i]-'0');
+		else;
+	}
+	printf("%i", cum);
+	return 0;
 }
