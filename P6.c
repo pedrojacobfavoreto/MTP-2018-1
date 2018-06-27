f883a36ec0db209a0ed2e83cc396e2b8eabb227d
pedrojacobfavoreto <38017495+pedrojacobfavoreto@users.noreply.github.com>
2018-06-27 09:24:23 -0300

    Update P6.c

--- P6.c
+++ P6.c
@@ -2,60 +2,26 @@
 //11721EAU003
 
 #include <stdio.h>
-
-int soma(int vet[20], int n);
-float media(int soma, float cont);
-void preenchezero(int vet[20]);
-
-int main()
-	{
-		int vet[20];
-		int i, somatorio, tam;
-		float cont;
-		printf("Digite a quantidade de termos desejados: ");
-		scanf("%d", &tam);
-		preenchezero(vet);
-		for(i=0; i<tam; i++)
-		{
-			printf("\nDigite o %d numero: ", i+1);
-			scanf("%d", &vet[i]);
-			getchar();
-			if(vet[i] != 0) 
-			cont++;
-		}
-		somatorio = (soma (vet, i) );
-		printf("\nA media dos numeros digitados eh: %f", media(somatorio, cont));
-		
-		return 0;
-		}
-		
-	
-void preenchezero(int vet[20])
-	{
-		int i;
-		for(i=0; i<20; i++)
-		{
-			vet[i] = 0;
-		}
-	}
-	
-int soma(int vet[20], int n) 
-	{
-		int s;
-	    if (n == 0) return 0;
-	    else{
-		      s = soma(vet, n-1);
-		      if (vet[n-1] > 0)
-			  	{
-				  s += vet[n-1];
-				}
-		      return (s);
-			}
-	}
-	
-float media(int soma, float cont)
-	{
-		float resul;
-		resul = soma/cont;
-		return resul;
-	}
+int sm(int len, int *v)
+{
+    if(len>0)
+        return *v+sm(len-1, v+1);
+    else
+        return 0.0;
+}
+float med(int x, int y)
+{
+    return (float)x/(float)y;
+}
+int main ()
+{
+	int vet[]={0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+	int tam=0, i=0;
+	printf("Quantidade a ser somada:\n");
+	scanf("%d", &tam);
+	printf("\nValores: \n");
+	for (i=0; i<tam; i++) 
+		scanf("%d", &vet[i]);
+	printf("\nmedia = %f", med(sm(tam, vet), tam));
+	return 0;
+}
